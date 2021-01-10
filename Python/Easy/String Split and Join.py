9916603f3d917f911b78ad86b991dbbe8ee7d77f
extremed1 <64342128+extremed1@users.noreply.github.com>
2021-01-10 18:10:24 -0500

    Update and rename String Split and Join to String Split and Join.py

--- Python/Easy/String Split and Join.py
+++ Python/Easy/String Split and Join.py
@@ -1,4 +1,4 @@
-You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.#
+#You are given a string. Split the string on a " " (space) delimiter and join using a - hyphen.#
 
 
 def split_and_join(line):
