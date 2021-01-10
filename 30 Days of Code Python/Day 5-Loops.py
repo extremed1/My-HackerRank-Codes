c78178abbadc7e4f0c76050f947cb7e9aa20fe1f
extremed1 <64342128+extremed1@users.noreply.github.com>
2021-01-10 18:55:30 -0500

    Update and rename Day 5: Loops to Day 5-Loops.py

--- 30 Days of Code Python/Day 5-Loops.py
+++ 30 Days of Code Python/Day 5-Loops.py
@@ -1,3 +1,5 @@
+#Given an integer,n , print its first 10  multiples. Each multiple n x i (where 1<=i<=10 ) should be printed on a new line in the form: n x i = result.
+
 import math
 import os
 import random
