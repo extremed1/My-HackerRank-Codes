974c8a2d989202fdd90dc28c4dbaae767f928eb1
extremed1 <64342128+extremed1@users.noreply.github.com>
2021-01-10 18:44:05 -0500

    Update and rename Day 1: Data Types to Day 1-Data Types.py

---
+++ 30 Days of Code Python/Day 1-Data Types.py
@@ -0,0 +1,27 @@
+#Complete the code in the editor below. The variables i ,d ,s are already declared and initialized for you. You must:
+
+#Declare 3 variables: one of type int, one of type double, and one of type String.
+#Read3  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your 3 variables.
+#Use the + operator to perform the following operations:
+#Print the sum of ilus your int variable on a new line.
+#Print the sum of dplus your double variable to a scale of one decimal place on a new line.
+#Concatenate swith the string you read as input and print the result on a new line.
+
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a = 0
+b = 0.0
+c = " "
+# Read and save an integer, double, and String to your variables.
+a = int(input())
+b = float(input())
+c = str(input())
+# Print the sum on a new line.
+print(i + a)
+print(d + b)
+print(s + c)
+
+
