6c6bbd9cbbaf904a11c9f1885a760b1afb771b39
extremed1 <64342128+extremed1@users.noreply.github.com>
2021-01-10 17:22:11 -0500

    Update and rename collections-Counter to collections-Counter.py

--- Python/Easy/collections-Counter.py
+++ Python/Easy/collections-Counter.py
@@ -1,5 +1,9 @@
+# Raghu is a shoe shop owner. His shop has X number of shoes.
+#He has a list containing the size of each shoe he has in his shop.
+#There are N number of customers who are willing to pay xi amount of money only if they get the shoe of their desired size.
 
-```
+#Your task is to compute how much money Raghu  earned
+
 from collections import Counter
 
 
